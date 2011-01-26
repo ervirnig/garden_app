265e165c12f6af9cbede82093c0810723cf3be08
MATC Student <student@PHPubuntu.(none)>
2011-01-25 19:16:21 -0600

    Instal commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GardenApp::Application.config.secret_token = '9ec29b3770ac9e978ea394876bc73eb4c82a4186161e85290cbacaa3d77c28530f522ba822a42f82be969622be049ff8672666c72c0ea53b0db05f5b45ba0404'
